a3e89c8bb7666258ec38426ff95829a98a952301
LeanArbelo22 <leo.arbelo22@gmail.com>
2022-09-22 01:29:51 -0300

    social media animate logo 3d

--- src/Components/SocialNetworks/SocialNetworks.jsx
+++ src/Components/SocialNetworks/SocialNetworks.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './SocialNetwork.scss';
-import { BsInstagram, BsWhatsapp, BsLinkedin } from 'react-icons/bs';
+import { FaInstagram, FaWhatsapp, FaLinkedinIn } from 'react-icons/fa';
 
 function SocialNetworks() {
+  const icons = [
+    {
+      link: '/instagram',
+      icon: <FaInstagram />
+    },
+    {
+      link: '/linkedin',
+      icon: <FaLinkedinIn />
+    },
+    {
+      link: '/whatsapp',
+      icon: <FaWhatsapp />
+    }
+  ]
+
   return (
-    <div className={`icons`}>
-        <Link to='/whatsapp'><BsWhatsapp /></Link>
-        <Link to='/instagram'><BsInstagram /></Link>
-        <Link to='/linkedin'><BsLinkedin /></Link>
-    </div>
+    <ul className={`icons`}>
+      {
+        icons.map(icon => {
+          return (
+            <li key={icon}>
+              <Link to={icon.link}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span>{icon.icon}</span>
+              </Link>
+            </li>
+          )
+        })
+      }
+
+
+
+
+
+    </ul>
   )
 }
 
